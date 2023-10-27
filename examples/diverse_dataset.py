3db8bad41de176af65fc62c7017ce64f83329ec1
cseto23 <cseto@asu.edu>
2023-10-26 20:12:07 -0700

    Merge branch 'main' of https://github.com/cseto23/llama-recipes-CSE576

--- examples/diverse_dataset.py
+++ examples/diverse_dataset.py
@@ -1,16 +1,84 @@
 import json
 from datasets import load_dataset
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+# For dataset details visit: https://crfm.stanford.edu/2023/03/13/alpaca.html
+
+import copy
+import json
+
+import torch
+from torch.utils.data import Dataset
+
+
+PROMPT_DICT = {
+    "prompt_input": (
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+    ),
+    "prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Response:"
+    ),
+}
+
+
+class DiverseDataset(Dataset):
+    def __init__(self, dataset_config, tokenizer, split, max_words=30):
+        fp = open('/home/mrlunt/llama-recipes-CSE576/examples/diverse_examples.json', 'r')
+        div_ex = json.load(fp)
+        fp.close()
+        div_ex = div_ex['examples']
+        alpaca_ds = load_dataset('tatsu-lab/alpaca', split="train")
+        self.ann = []
+        for i in div_ex[:200] if split == "val" else div_ex[200:]:
+            self.ann.append(alpaca_ds[i])
+        self.max_words = max_words
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.ann)
+
+    def __getitem__(self, index):
+        IGNORE_INDEX = -100  # The default setting in CrossEntropyLoss
+        ann = self.ann[index]
+        if ann.get("input", "") == "":
+            prompt = PROMPT_DICT["prompt_no_input"].format_map(ann)
+        else:
+            prompt = PROMPT_DICT["prompt_input"].format_map(ann)
+        example = prompt + ann["output"]
+        prompt = torch.tensor(
+            self.tokenizer.encode(prompt), dtype=torch.int64
+        )
+        example = self.tokenizer.encode(example)
+        example.append(self.tokenizer.eos_token_id)
+        example = torch.tensor(
+            example, dtype=torch.int64
+        )
+        padding = self.max_words - example.shape[0]
+        if padding > 0:
+            example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
+        elif padding < 0:
+            example = example[: self.max_words]
+        labels = copy.deepcopy(example)
+        labels[: len(prompt)] = -1
+        example_mask = example.ge(0)
+        label_mask = labels.ge(0)
+        example[~example_mask] = 0
+        labels[~label_mask] = IGNORE_INDEX
+        example_mask = example_mask.float()
+        label_mask = label_mask.float()
+
+        return {
+            "input_ids": example,
+            "labels": labels,
+            "attention_mask":example_mask,
+        }
+
+
 def get_custom_dataset(dataset_config, tokenizer, split: str):
-    fp = open('/home/cseto/llama-recipes-CSE576/examples/diverse_examples.json','r')
-    div_ex = json.load(fp)
-    fp.close()
-
-    div_ex = div_ex['examples']
-
-    alpaca_ds = load_dataset('tatsu-lab/alpaca',split=split)
-    diverse_ds = []
-    for i in div_ex:
-        diverse_ds.append(alpaca_ds[i]['text'])
-    
-    return diverse_ds
+    return DiverseDataset(dataset_config, tokenizer, split)
