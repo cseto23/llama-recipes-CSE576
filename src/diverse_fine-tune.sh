52dc2226b3b740a0a1db23b8cf717989c872fa7c
[ Christian Seto - Ira A Fulton Engineering - Graduate Student - cseto ] <cseto@login01.sol.rc.asu.edu>
2023-11-27 16:58:38 -0700

    created bash scripts for fine-tuning

---
+++ src/diverse_fine-tune.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -N 1 # number of nodes
+#SBATCH -c 1 # number of cores
+#SBATCH -t 0-3:00:00 # time in d-hh:mm:ss
+#SBATCH --gres gpu:a100:1
+
+module load mamba
+
+source activate NLP_ENV
+
+python3 -m llama_recipes.finetuning --dataset "diverse_dataset" --custom_dataset.file $1 --use_peft --save_model --peft_method lora --quantization --model_name $2 \
+ --output_dir $3
