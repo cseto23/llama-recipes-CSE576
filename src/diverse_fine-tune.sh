978118e91b3b925ac73dac5312a54d141f436b09
[ Christian Seto - Ira A Fulton Engineering - Graduate Student - cseto ] <cseto@login01.sol.rc.asu.edu>
2023-12-03 23:34:21 -0700

    Some final push to remove slurms and update diverse_fine-tune.sh

--- src/diverse_fine-tune.sh
+++ src/diverse_fine-tune.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#SBATCH -p general
 #SBATCH -N 1 # number of nodes
 #SBATCH -c 1 # number of cores
 #SBATCH -t 0-3:00:00 # time in d-hh:mm:ss
@@ -9,5 +10,4 @@ module load mamba
 
 source activate NLP_ENV
 
-python3 -m llama_recipes.finetuning --dataset "diverse_dataset" --custom_dataset.file $1 --use_peft --save_model --peft_method lora --quantization --num_epochs 5 --model_name $2 \
- --output_dir $3
+python3 -m llama_recipes.finetuning --dataset "diverse_dataset" --custom_dataset.file $1 --use_peft --save_model --peft_method lora --quantization --num_epochs 5 --model_name $2 --output_dir $3
